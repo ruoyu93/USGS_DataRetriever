c0daa45cedb10591c62baeed1cc3cde7e2ebf0ff
ruoyu93 <40345757+ruoyu93@users.noreply.github.com>
2020-08-04 11:28:18 -0400

    Update JupyterNotebook

--- Unit_test.py
+++ Unit_test.py
@@ -11,3 +11,19 @@ from USGS_FlowData_utils import *
 
 class USGS_DataRetriever_Test(unittest.TestCase):
     
+    
+data = pd.read_csv("va_gages.csv", dtype=str)
+trend_out = pd.DataFrame(columns = ['GageID','trend result','slope'])
+count = 0
+for i in range(len(data)):
+    rcode = data["SOURCE_FEA"][i]
+    try:
+        site = USGS_Gage_DataRetriever(rcode,  metric=False)
+        trend, slope, rts, rmk, reason = site.trendTest('Y', 10, 0.05)
+        data.loc[count] = [rcode, trend, slope]
+        
+        count +=1
+        # site.getGeoMetaData()
+    except:
+        print("")
+        continue
